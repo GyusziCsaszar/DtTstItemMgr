6d559323c59c76d8df840ae687214b754e50e586
GyusziCsaszar <gyuszi.csaszar@gmail.com>
2020-06-23 14:06:47 +0200

    Initial commit.

---
+++ uFrmFDB.dfm
@@ -0,0 +1,370 @@
+object FrmFDB: TFrmFDB
+  Left = 0
+  Top = 0
+  Caption = 'FrmFDB'
+  ClientHeight = 364
+  ClientWidth = 602
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poOwnerFormCenter
+  OnShow = FormShow
+  DesignSize = (
+    602
+    364)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lblCaption: TLabel
+    Left = 8
+    Top = 9
+    Width = 194
+    Height = 25
+    Caption = 'Connect to Database'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -21
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object panLower: TPanel
+    Left = 0
+    Top = 43
+    Width = 604
+    Height = 322
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    Color = clWindow
+    ParentBackground = False
+    TabOrder = 0
+    DesignSize = (
+      604
+      322)
+    object lblUser: TLabel
+      Left = 216
+      Top = 96
+      Width = 26
+      Height = 13
+      Caption = 'User:'
+    end
+    object lblPw: TLabel
+      Left = 192
+      Top = 135
+      Width = 50
+      Height = 13
+      Caption = 'Password:'
+    end
+    object lblDb: TLabel
+      Left = 8
+      Top = 8
+      Width = 219
+      Height = 13
+      Caption = 'Firebird Database Connect Strings (from INI):'
+      Visible = False
+    end
+    object edUser: TEdit
+      Left = 248
+      Top = 93
+      Width = 121
+      Height = 21
+      TabOrder = 0
+    end
+    object edPw: TEdit
+      Left = 248
+      Top = 132
+      Width = 121
+      Height = 21
+      PasswordChar = '*'
+      TabOrder = 1
+    end
+    object btnLogin: TButton
+      Left = 216
+      Top = 176
+      Width = 75
+      Height = 25
+      Caption = 'Login'
+      TabOrder = 2
+      OnClick = btnLoginClick
+    end
+    object btnClose: TButton
+      Left = 313
+      Top = 176
+      Width = 75
+      Height = 25
+      Caption = 'Close'
+      TabOrder = 3
+      OnClick = btnCloseClick
+    end
+    object cbbDb: TComboBox
+      Left = 8
+      Top = 27
+      Width = 548
+      Height = 21
+      Anchors = [akLeft, akTop, akRight]
+      TabOrder = 4
+      Visible = False
+    end
+    object chbServerCharsetUtf8: TCheckBox
+      Left = 8
+      Top = 54
+      Width = 140
+      Height = 17
+      Caption = 'Server CharSet = UTF8'
+      Checked = True
+      State = cbChecked
+      TabOrder = 5
+      Visible = False
+    end
+    object btnDbOpen: TButton
+      Left = 562
+      Top = 25
+      Width = 33
+      Height = 25
+      Anchors = [akTop, akRight]
+      Caption = '...'
+      TabOrder = 7
+      Visible = False
+      OnClick = btnDbOpenClick
+    end
+    object grpIsql: TGroupBox
+      Left = 8
+      Top = 77
+      Width = 169
+      Height = 124
+      Caption = 'Firebird Isql tool'
+      TabOrder = 8
+      Visible = False
+      object btnIsqlTest: TButton
+        Left = 12
+        Top = 25
+        Width = 65
+        Height = 25
+        Caption = 'Test Isql'
+        TabOrder = 0
+        OnClick = btnIsqlTestClick
+      end
+    end
+    object chbIsqlVisible: TCheckBox
+      Left = 95
+      Top = 102
+      Width = 74
+      Height = 17
+      Caption = 'Show Isql'
+      TabOrder = 9
+    end
+    object btnCreTblSample: TButton
+      Left = 452
+      Top = 144
+      Width = 143
+      Height = 57
+      Anchors = [akTop, akRight]
+      Caption = 'Create Table'
+      Enabled = False
+      TabOrder = 10
+      Visible = False
+      WordWrap = True
+      OnClick = btnCreTblSampleClick
+    end
+    object chbAutoLogin: TCheckBox
+      Left = 248
+      Top = 54
+      Width = 97
+      Height = 17
+      Caption = 'Auto Login'
+      TabOrder = 11
+      OnClick = chbAutoLoginClick
+    end
+    object btnCrePrdTbls: TButton
+      Left = 452
+      Top = 80
+      Width = 143
+      Height = 58
+      Anchors = [akTop, akRight]
+      Caption = 
+        'Create Product Tables (in Empty DB or to add product to an exist' +
+        'ing DB)'
+      Enabled = False
+      TabOrder = 12
+      WordWrap = True
+      OnClick = btnCrePrdTblsClick
+    end
+    object chbDoDbUpdate: TCheckBox
+      Left = 452
+      Top = 54
+      Width = 97
+      Height = 17
+      Anchors = [akTop, akRight]
+      Caption = 'Do DB Update'
+      Checked = True
+      State = cbChecked
+      TabOrder = 13
+    end
+    object btnIsqlShowDb: TButton
+      Left = 20
+      Top = 164
+      Width = 147
+      Height = 25
+      Caption = 'SHOW Database'
+      TabOrder = 14
+      OnClick = btnIsqlShowDbClick
+    end
+    object tsDev: TTabSet
+      Left = 0
+      Top = 216
+      Width = 602
+      Height = 79
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -11
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      SoftTop = True
+      Style = tsSoftTabs
+      Tabs.Strings = (
+        'LOG'
+        'SQL Editor')
+      TabIndex = 0
+      Visible = False
+      OnChange = tsDevChange
+    end
+    object lbLog: TListBox
+      Left = 14
+      Top = 240
+      Width = 575
+      Height = 38
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      ItemHeight = 13
+      TabOrder = 16
+      Visible = False
+      OnDblClick = lbLogDblClick
+      ExplicitHeight = 57
+    end
+    object moSql: TMemo
+      Left = 14
+      Top = 254
+      Width = 575
+      Height = 24
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      ScrollBars = ssBoth
+      TabOrder = 17
+      Visible = False
+      ExplicitHeight = 43
+    end
+    object btnSqlOpen: TButton
+      Left = 127
+      Top = 223
+      Width = 75
+      Height = 25
+      Caption = 'Open Query'
+      Enabled = False
+      TabOrder = 18
+      Visible = False
+      OnClick = btnSqlOpenClick
+    end
+    object btnSqlOpenSample: TButton
+      Left = 208
+      Top = 223
+      Width = 57
+      Height = 25
+      Caption = 'Sample'
+      TabOrder = 19
+      Visible = False
+      OnClick = btnSqlOpenSampleClick
+    end
+    object btnIsqlExec: TButton
+      Left = 313
+      Top = 223
+      Width = 75
+      Height = 25
+      Caption = 'Isql Exec'
+      TabOrder = 20
+      Visible = False
+      OnClick = btnIsqlExecClick
+    end
+    object btnIsqlExecSample: TButton
+      Left = 394
+      Top = 223
+      Width = 57
+      Height = 25
+      Caption = 'Sample'
+      TabOrder = 21
+      Visible = False
+      OnClick = btnIsqlExecSampleClick
+    end
+    object edTerm: TEdit
+      Left = 470
+      Top = 225
+      Width = 50
+      Height = 21
+      Hint = 'Firebird Isql SET TERM ?'
+      Alignment = taCenter
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clRed
+      Font.Height = -11
+      Font.Name = 'Tahoma'
+      Font.Style = [fsBold]
+      ParentFont = False
+      ParentShowHint = False
+      ShowHint = True
+      TabOrder = 22
+      Text = ';'
+      Visible = False
+    end
+    object btnIsqlCreateDb: TButton
+      Left = 20
+      Top = 133
+      Width = 147
+      Height = 25
+      Caption = 'CREATE Database (UTF8)'
+      TabOrder = 23
+      OnClick = btnIsqlCreateDbClick
+    end
+    object panDbInfo: TPanel
+      Left = 0
+      Top = 293
+      Width = 604
+      Height = 29
+      Alignment = taLeftJustify
+      Anchors = [akLeft, akRight, akBottom]
+      BevelEdges = []
+      BevelInner = bvRaised
+      BorderStyle = bsSingle
+      Color = clInfoBk
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -12
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentBackground = False
+      ParentFont = False
+      TabOrder = 6
+      Visible = False
+      ExplicitTop = 312
+    end
+  end
+  object panAdminMode: TPanel
+    Left = 372
+    Top = 24
+    Width = 217
+    Height = 29
+    Anchors = [akTop, akRight]
+    BevelEdges = []
+    BevelInner = bvRaised
+    BorderStyle = bsSingle
+    Caption = 'Database Administration Mode'
+    Color = clInfoBk
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentBackground = False
+    ParentFont = False
+    TabOrder = 1
+    Visible = False
+  end
+end
---
+++ uFrmFDB.pas
@@ -0,0 +1,688 @@
+unit uFrmFDB;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  { DtTst Units: } uDtTstApp,
+  Vcl.StdCtrls, Vcl.ExtCtrls,
+  Data.SqlExpr, Vcl.Tabs;
+
+type
+  TFrmFDB = class(TForm)
+    panLower: TPanel;
+    lblCaption: TLabel;
+    lblUser: TLabel;
+    edUser: TEdit;
+    lblPw: TLabel;
+    edPw: TEdit;
+    btnLogin: TButton;
+    btnClose: TButton;
+    cbbDb: TComboBox;
+    chbServerCharsetUtf8: TCheckBox;
+    panAdminMode: TPanel;
+    lblDb: TLabel;
+    panDbInfo: TPanel;
+    btnDbOpen: TButton;
+    grpIsql: TGroupBox;
+    btnIsqlTest: TButton;
+    chbIsqlVisible: TCheckBox;
+    btnCreTblSample: TButton;
+    chbAutoLogin: TCheckBox;
+    btnCrePrdTbls: TButton;
+    chbDoDbUpdate: TCheckBox;
+    btnIsqlShowDb: TButton;
+    tsDev: TTabSet;
+    lbLog: TListBox;
+    moSql: TMemo;
+    btnSqlOpen: TButton;
+    btnSqlOpenSample: TButton;
+    btnIsqlExec: TButton;
+    btnIsqlExecSample: TButton;
+    edTerm: TEdit;
+    btnIsqlCreateDb: TButton;
+    procedure btnCloseClick(Sender: TObject);
+    procedure btnLoginClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure btnDbOpenClick(Sender: TObject);
+    procedure btnIsqlTestClick(Sender: TObject);
+    procedure btnCreTblSampleClick(Sender: TObject);
+    procedure chbAutoLoginClick(Sender: TObject);
+    procedure btnCrePrdTblsClick(Sender: TObject);
+    procedure btnIsqlShowDbClick(Sender: TObject);
+    procedure tsDevChange(Sender: TObject; NewTab: Integer;
+      var AllowChange: Boolean);
+    procedure btnIsqlExecSampleClick(Sender: TObject);
+    procedure btnIsqlExecClick(Sender: TObject);
+    procedure btnSqlOpenSampleClick(Sender: TObject);
+    procedure btnSqlOpenClick(Sender: TObject);
+    procedure lbLogDblClick(Sender: TObject);
+    procedure btnIsqlCreateDbClick(Sender: TObject);
+  private
+    { Private declarations }
+    m_oApp: TDtTstApp;
+    m_oConAnsi: TSQLConnection;
+    m_oConUtf8: TSQLConnection;
+    m_oCon    : TSQLConnection;
+  public
+    { Public declarations }
+    SQL_OpenSelect: string;
+
+    constructor Create(AOwner: TComponent; oApp: TDtTstApp); reintroduce;
+    destructor Destroy(); override;
+
+    function GetCon(): TSQLConnection;
+    property CON: TSQLConnection read GetCon;
+
+    function GetDbInfo() : string;
+
+    procedure IsqlOpen(bConnectDB: Boolean; sStatements, sTerm: string);
+
+    procedure AttachDbItemManager(bForceUpdate: Boolean);
+
+    function ShowModal(oConAnsi, oConUtf8: TSQLConnection) : integer; reintroduce;
+
+  end;
+
+var
+  FrmFDB: TFrmFDB;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  { DtTst Units: } uDtTstConsts, uDtTstUtils, uDtTstWin, uDtTstFirebird, uDtTstDb, uDtTstDbItemMgr, uDtTstAppDb,
+  { DtTst Forms: } uFrmProgress,
+  System.IOUtils, Vcl.Clipbrd, StrUtils;
+
+constructor TFrmFDB.Create(AOwner: TComponent; oApp: TDtTstApp);
+begin
+
+  SQL_OpenSelect := '';
+
+  m_oApp := oApp;
+
+  inherited Create(AOwner);
+
+  LoadFormSizeReg(self, csCOMPANY, csPRODUCT, 'FrmFDB');
+
+  // Registry...
+  chbAutoLogin       .Checked := LoadBooleanReg(csCOMPANY, csPRODUCT, 'Settings\DB', 'AutoLogin'   , False);
+
+  // DB Info from INI file...
+  cbbDb.Text := '';
+  cbbDb.Items.Clear();
+
+  (m_oApp as TDtTstAppDb).DB.GetConnectStrings(cbbDb, nil {asConnectStrings});
+
+  chbServerCharsetUtf8.Checked := (m_oApp as TDtTstAppDb).DB.UTF8;
+
+  m_oApp.LOG.LogLIFE('TFrmFDB.Create');
+end;
+
+destructor TFrmFDB.Destroy();
+begin
+  m_oApp.LOG.LogLIFE('TFrmFDB.Destroy');
+
+  SaveFormSizeReg(self, csCOMPANY, csPRODUCT, 'FrmFDB');
+
+  m_oApp := nil; // ATTN: Do not Free here!
+
+  inherited Destroy();
+end;
+
+procedure TFrmFDB.FormShow(Sender: TObject);
+begin
+
+  self.Caption := Application.Title;
+
+  btnCreTblSample.Caption := btnCreTblSample.Caption + ' "' + csDB_TBL_SAMPLE + '"';
+
+  if not m_oApp.ADMIN_MODE then
+  begin
+
+    // ATTN: Force event handler...
+  //tsDev.TabIndex := 1; tsDev.TabIndex := 0;
+    tsDev.TabIndex := 0; tsDev.TabIndex := 1;
+
+  end;
+
+  // ATTN!!! Form has to work without calling to FormShow!!!
+
+end;
+
+procedure TFrmFDB.btnCloseClick(Sender: TObject);
+begin
+  if panAdminMode.Visible and (not btnLogin.Enabled) then
+  begin
+    ModalResult := mrOk;
+  end
+  else
+  begin
+    ModalResult := mrCancel;
+  end;
+end;
+
+function TFrmFDB.GetCon(): TSQLConnection;
+begin
+  Result := m_oCon;
+end;
+
+procedure TFrmFDB.btnCrePrdTblsClick(Sender: TObject);
+var
+  sDbInfo: string;
+begin
+  m_oApp.LOG.LogUI('TFrmFDB.btnCrePrdTblsClick BEGIN');
+
+  if (not Assigned((m_oApp as TDtTstAppDb).DB.SQLConnection)) or (not (m_oApp as TDtTstAppDb).DB.SQLConnection.Connected) then
+  begin
+    WarningMsgDlg('No Database is open!');
+    Exit;
+  end;
+
+  if not (m_oApp as TDtTstAppDb).DB.ADM_DbInfProduct.IsEmpty() then
+  begin
+    if (m_oApp as TDtTstAppDb).DB.ADM_DbInfProduct = csPRODUCT_FULL then
+    begin
+      WarningMsgDlg('Database already contains Tables for Product "' + csPRODUCT_FULL + '"!');
+    end
+    else
+    begin
+      ErrorMsgDlg('Database contains Tables for DIFFERENT Product "' + (m_oApp as TDtTstAppDb).DB.ADM_DbInfProduct + '"!' +
+                  CHR(10) + CHR(10) + 'No mixed Products are allowed in a single Database!');
+    end;
+
+    Exit;
+  end;
+
+  if (m_oApp as TDtTstAppDb).DB.GetTableCount() > 0 then
+  begin
+    if not QuestionMsgDlg('Database already has ' + IntToStr((m_oApp as TDtTstAppDb).DB.GetTableCount()) + ' Table(s) and is not Empty!' +
+          CHR(10) + CHR(10) + 'Do you want to create Tables for Product "' + csCOMPANY + csPRODUCT + '"?') then
+    begin
+      Exit;
+    end;
+  end;
+
+  try
+
+    AttachDbItemManager(True {bForceUpdate});
+
+  except
+    on exc : Exception do
+    begin
+      m_oApp.LOG.LogERROR(exc);
+      ErrorMsgDlg('Error: ' + exc.ClassName + ' - ' + exc.Message);
+    end;
+  end;
+
+  m_oApp.LOG.LogUI('TFrmFDB.btnCrePrdTblsClick END');
+end;
+
+procedure TFrmFDB.AttachDbItemManager(bForceUpdate: Boolean);
+var
+  oDbItemMgr: TDtTstDbItemMgr;
+  sDbInfo: string;
+  frmPrs: TFrmProgress;
+begin
+
+  // ATTN: In case of (one-and-only) Known DB
+  //       OR FORCED (!)...
+  if bForceUpdate or ((m_oApp as TDtTstAppDb).DB.ADM_DbInfProduct = csPRODUCT_FULL) then
+  begin
+
+    // Replace oDb with oDbItemMgr...
+    oDbItemMgr := TDtTstDbItemMgr.Create(m_oApp.LOG, (m_oApp as TDtTstAppDb).DB);
+
+    (m_oApp as TDtTstAppDb).SetDb(oDbItemMgr);
+
+    if chbDoDbUpdate.Checked then
+    begin
+
+      frmPrs := TFrmProgress.Create(self, m_oApp);
+      try
+
+        oDbItemMgr.ADM_DoDbUpdates(frmPrs);
+
+        frmPrs.Done();
+        while frmPrs.Visible do Application.ProcessMessages;
+
+      finally
+        frmPrs.Close();
+        FreeAndNil(frmPrs);
+      end;
+
+    end;
+
+    sDbInfo := 'LoginUsername = "' + (m_oApp as TDtTstAppDb).DB.LoginUser + '"';
+    sDbInfo := sDbInfo + ', DtTstDb( ADM Version = ' + IntToStr((m_oApp as TDtTstAppDb).DB.ADM_DbInfVersion_ADM) +
+                ', Product = "' + (m_oApp as TDtTstAppDb).DB.ADM_DbInfProduct + '"' +
+                ', Product Version = ' + IntToStr((m_oApp as TDtTstAppDb).DB.ADM_DbInfVersion_PRD) +
+                ', UserID = ' + IntToStr(oDbItemMgr.ADM_UserID) + ' )';
+    sDbInfo := sDbInfo + ', DtTstDbItemMgr(  )';
+
+    panDbInfo.Caption := sDbInfo;
+
+  end;
+
+end;
+
+procedure TFrmFDB.btnCreTblSampleClick(Sender: TObject);
+var
+  oDb: TDtTstDb;
+begin
+  m_oApp.LOG.LogUI('TFrmFDB.btnCreTblSampleClick BEGIN');
+
+  // try SRC: https://stackoverflow.com/questions/6601147/how-to-correctly-write-try-finally-except-statements
+  try
+    try
+      oDb := TDtTstDb.Create(m_oApp.LOG, '');
+
+      oDb.META_CreateTable_SAMPLE((m_oApp as TDtTstAppDb).DB.SQLConnection.DBXConnection);
+
+      InfoMsgDlg('You have created table "' + csDB_TBL_SAMPLE + '"!');
+
+    finally
+      FreeAndNil(oDb);
+    end;
+  except
+    on exc : Exception do
+    begin
+      m_oApp.LOG.LogERROR(exc);
+      ErrorMsgDlg('Error: ' + exc.ClassName + ' - ' + exc.Message);
+    end;
+  end;
+
+  m_oApp.LOG.LogUI('TFrmFDB.btnCreTblSampleClick END');
+end;
+
+procedure TFrmFDB.btnDbOpenClick(Sender: TObject);
+// SRC: http://www.delphibasics.co.uk/RTL.asp?Name=topendialog
+var
+  frmOf: TOpenDialog;
+begin
+
+  frmOf := TOpenDialog.Create(self);
+  try
+
+    // Set up the starting directory to be the current one
+    //frmOf.InitialDir := GetCurrentDir();
+
+    // Only allow existing files to be selected
+    frmOf.Options := [ofFileMustExist];
+
+    // Allow only .xyz files to be selected
+    frmOf.Filter := csFBRD_FDB_FILE_FILTER + '|All Files (*.*)|*.*';
+
+    // Select .xyz as the starting filter type
+    frmOf.FilterIndex := 1;
+
+    // Display the open file dialog
+    if not frmOf.Execute then
+    begin
+      Exit;
+    end;
+
+    cbbDb.Text := 'localhost:' + frmOf.FileName;
+
+  finally
+    FreeAndNil(frmOf);
+  end;
+
+end;
+
+function TFrmFDB.GetDbInfo() : string;
+begin
+  Result := panDbInfo.Caption;
+end;
+
+procedure TFrmFDB.IsqlOpen(bConnectDB: Boolean; sStatements, sTerm: string);
+var
+  sDb, sOutput: string;
+  frmPrs: TFrmProgress;
+begin
+
+  try
+
+    frmPrs := TFrmProgress.Create(self, m_oApp);
+    try
+
+      frmPrs.Show();
+      frmPrs.Init('Calling Firebird Isql tool');
+      frmPrs.SetProgressToMax();
+
+      frmPrs.AddStep('Starting Firebird Isql tool');
+
+      (m_oApp as TDtTstAppDb).DB.ConnectUser     := edUser.Text;
+      (m_oApp as TDtTstAppDb).DB.ConnectPassword := edPw  .Text;
+
+      if bConnectDB then
+        sDb := cbbDb.Text
+      else
+        sDb := '';
+
+      sOutput := ISQL_Execute(m_oApp.LOG, TPath.GetDirectoryName(Application.ExeName),
+                              (m_oApp as TDtTstAppDb).DB.IsqlPathChecked,
+                              sDb,
+                              (m_oApp as TDtTstAppDb).DB.ConnectUser, (m_oApp as TDtTstAppDb).DB.ConnectPassword,
+                              True {bGetOutput},
+                              chbIsqlVisible.Checked {bVisible},
+                              sStatements,
+                              sTerm);
+
+      frmPrs.AddStepEnd('Done!');
+
+      InfoMsgDlg('isql output (cch = ' + IntToStr(sOutput.Length) + '):' + CHR(10) + CHR(10) + sOutput);
+
+    finally
+      frmPrs.Close();
+      FreeAndNil(frmPrs);
+    end;
+
+  except
+    on exc : Exception do
+    begin
+      m_oApp.LOG.LogERROR(exc);
+      ErrorMsgDlg('Error: ' + exc.ClassName + ' - ' + exc.Message);
+    end;
+  end;
+
+end;
+
+procedure TFrmFDB.lbLogDblClick(Sender: TObject);
+begin
+  if lbLog.ItemIndex >= 0 then
+  begin
+    Clipboard.AsText := lbLog.Items[lbLog.ItemIndex];
+  end;
+end;
+
+procedure TFrmFDB.btnIsqlExecClick(Sender: TObject);
+begin
+  if not moSql.Lines.Text.IsEmpty() then
+  begin
+
+    if edTerm.Text = '' then edTerm.Text := ';';
+
+    if edTerm.Text <> ';' then
+    begin
+      if not QuestionMsgDlg('The default Isql TERM ";" has changed to "' + edTerm.Text + '"!' + CHR(13) + CHR(10) +
+                            CHR(13) + CHR(10) + 'Do you want to conitune?') then
+      begin
+        Exit;
+      end;
+    end;
+
+    IsqlOpen(True {bConnectDB}, moSql.Lines.Text, edTerm.Text);
+  end;
+end;
+
+procedure TFrmFDB.btnIsqlExecSampleClick(Sender: TObject);
+begin
+  edTerm.Text := '!!';
+  moSql.Lines.Text := 'select current_timestamp from RDB$DATABASE!!';
+
+  if btnIsqlExec.Enabled then btnIsqlExec.Click();
+
+  edTerm.Text := ';';
+  moSql.Lines.Text := 'select current_timestamp from RDB$DATABASE;';
+end;
+
+procedure TFrmFDB.btnIsqlCreateDbClick(Sender: TObject);
+var
+  sStatements, sTerm: string;
+begin
+  m_oApp.LOG.LogUI('btnIsqlCreateDbClick BEGIN');
+
+  if TComboBox_Text(cbbDb).IsEmpty() then
+  begin
+    WarningMsgDlg('No database specified!');
+    Exit;
+  end;
+
+  if not QuestionMsgDlg('Creating new Database "' + cbbDb.Text + '"!' + CHR(10) + CHR(10) +
+                        //'WARNING: User and password will be present in TEMP file for short!' + CHR(10) + CHR(10) +
+                        'Do you want to continue?') then
+  begin
+    Exit;
+  end;
+
+
+  sTerm := '';
+  sStatements := 'CREATE DATABASE ' + '''' + cbbDb.Text + '''' +
+                 {' USER ' + edUser.Text +
+                 ' PASSWORD ' + edPw.Text + }
+                 ' PAGE_SIZE 4096' +
+                 ' DEFAULT CHARACTER SET ' + 'UTF8' +
+                 {' DEFAULT COLLATION ' + 'UTF8' +} // NO DEFAULT COLLATION
+                 ';';
+
+  IsqlOpen(False {bConnectDB}, sStatements, sTerm);
+
+  m_oApp.LOG.LogUI('btnIsqlCreateDbClick END');
+end;
+
+procedure TFrmFDB.btnIsqlShowDbClick(Sender: TObject);
+var
+  sStatements, sTerm: string;
+begin
+  m_oApp.LOG.LogUI('TFrmFDB.btnIsqlShowDbClick BEGIN');
+
+  sTerm := '';
+  sStatements := 'SHOW DATABASE;';
+
+  IsqlOpen(True {bConnectDB}, sStatements, sTerm);
+
+  m_oApp.LOG.LogUI('TFrmFDB.btnIsqlShowDbClick END');
+end;
+
+procedure TFrmFDB.btnIsqlTestClick(Sender: TObject);
+var
+  sStatements, sTerm: string;
+begin
+  m_oApp.LOG.LogUI('TFrmFDB.btnIsqlTestClick BEGIN');
+
+  sTerm := '';
+  sStatements := '';
+
+  IsqlOpen(True {bConnectDB}, sStatements, sTerm);
+
+  m_oApp.LOG.LogUI('TFrmFDB.btnIsqlTestClick END');
+end;
+
+procedure TFrmFDB.chbAutoLoginClick(Sender: TObject);
+begin
+  m_oApp.LOG.LogUI('TFrmFDB.chbAutoConnectClick BEGIN');
+
+  SaveBooleanReg(csCOMPANY, csPRODUCT, 'Settings\DB', 'AutoLogin', chbAutoLogin.Checked);
+
+  m_oApp.LOG.LogUI('TFrmFDB.chbAutoConnectClick END');
+end;
+
+procedure TFrmFDB.btnLoginClick(Sender: TObject);
+var
+  sDbInfo: string;
+begin
+
+  try
+
+    if chbServerCharSetUtf8.Checked then
+    begin
+      m_oCon := m_oConUtf8;
+    end
+    else
+    begin
+      m_oCon := m_oConAnsi;
+    end;
+
+    (m_oApp as TDtTstAppDb).DB.ConnectUser     := edUser.Text;
+    (m_oApp as TDtTstAppDb).DB.ConnectPassword := edPw  .Text;
+
+    (m_oApp as TDtTstAppDb).DB.ConnectString := cbbDb.Text;
+    (m_oApp as TDtTstAppDb).DB.Connect(m_oCon);
+
+    sDbInfo := 'LoginUsername = "' + (m_oApp as TDtTstAppDb).DB.LoginUser + '"';
+    sDbInfo := sDbInfo + ', DtTstDb( ADM Version = ' + IntToStr((m_oApp as TDtTstAppDb).DB.ADM_DbInfVersion_ADM) +
+                ', Product = "' + (m_oApp as TDtTstAppDb).DB.ADM_DbInfProduct + '" )';
+
+    panDbInfo.Caption := sDbInfo;
+
+    // ATTN: Do not force!!! Attach only known DB here!!!
+    AttachDbItemManager(False {bForce});
+
+    // Connected...
+    btnCrePrdTbls       .Enabled := ((m_oApp as TDtTstAppDb).DB.ADM_DbInfProduct <> csPRODUCT_FULL);
+    cbbDb               .Enabled := False;
+    btnDbOpen           .Enabled := False;
+    chbServerCharSetUtf8.Enabled := False;
+    btnIsqlCreateDb     .Enabled := False;
+
+    btnCreTblSample     .Enabled := True;
+    btnSqlOpen          .Enabled := True;
+
+    if panAdminMode.Visible then
+    begin
+      btnLogin.Enabled := False;
+    end
+    else
+    begin
+      ModalResult := mrOk;
+    end;
+
+  except
+    on exc : Exception do
+    begin
+      m_oApp.LOG.LogERROR(exc);
+      ErrorMsgDlg('Error: ' + exc.ClassName + ' - ' + exc.Message);
+    end;
+  end;
+
+end;
+
+procedure TFrmFDB.btnSqlOpenClick(Sender: TObject);
+var
+  sSql: string;
+begin
+  if not moSql.Lines.Text.IsEmpty() then
+  begin
+
+    sSql := moSql.Lines.Text;
+
+    if StartsText('SELECT ', TRIM(sSql.ToUpper())) then
+    begin
+
+      {
+      OpenSql('SQL Editor', sSql);
+      }
+
+      // Syntax Check...
+      (m_oApp as TDtTstAppDb).DB.ExecuteSQL(nil {nil = DO Transaction}, sSql);
+
+      if QuestionMsgDlg('To open Select query this window needs to close!' + CHR(10) + CHR(10) +
+                        'Do you want to continue?') then
+      begin
+
+        SQL_OpenSelect := sSql;
+
+        ModalResult := mrOk;
+
+      end;
+
+    end
+    else
+    begin
+      (m_oApp as TDtTstAppDb).DB.ExecuteSQL(nil {nil = DO Transaction}, sSql);
+    end;
+
+  end;
+end;
+
+procedure TFrmFDB.btnSqlOpenSampleClick(Sender: TObject);
+begin
+
+  moSql.Lines.Text := 'select current_timestamp from RDB$DATABASE';
+
+  // ATTN: DO NOT!!! Closes form when sql is select!
+  //if btnSqlOpen.Enabled then btnSqlOpen.Click();
+
+end;
+
+function TFrmFDB.ShowModal(oConAnsi, oConUtf8: TSQLConnection) : integer;
+begin
+  Result     := mrCancel;
+
+  m_oApp.LOG.m_lbLogView := lbLog;
+  try
+
+    m_oConAnsi := oConAnsi;
+    m_oConUtf8 := oConUtf8;
+
+    m_oCon     := nil;
+
+    panAdminMode          .Visible := m_oApp.ADMIN_MODE;
+    lblDb                 .Visible := m_oApp.ADMIN_MODE;
+    cbbDb                 .Visible := m_oApp.ADMIN_MODE;
+    btnDbOpen             .Visible := m_oApp.ADMIN_MODE;
+    chbServerCharsetUtf8  .Visible := m_oApp.ADMIN_MODE;
+    panDbInfo             .Visible := m_oApp.ADMIN_MODE;
+    grpIsql               .Visible := m_oApp.ADMIN_MODE;
+    chbIsqlVisible        .Visible := m_oApp.ADMIN_MODE;
+    btnIsqlCreateDb       .Visible := m_oApp.ADMIN_MODE;
+    btnIsqlShowDb         .Visible := m_oApp.ADMIN_MODE;
+    chbDoDbUpdate         .Visible := m_oApp.ADMIN_MODE;
+    btnCrePrdTbls         .Visible := m_oApp.ADMIN_MODE;
+    btnCreTblSample       .Visible := m_oApp.ADMIN_MODE;
+    tsDev                 .Visible := m_oApp.ADMIN_MODE;
+    moSql                 .Visible := m_oApp.ADMIN_MODE;
+    btnSqlOpen            .Visible := m_oApp.ADMIN_MODE;
+    btnSqlOpenSample      .Visible := m_oApp.ADMIN_MODE;
+    btnIsqlExec           .Visible := m_oApp.ADMIN_MODE;
+    btnIsqlExecSample     .Visible := m_oApp.ADMIN_MODE;
+    edTerm                .Visible := m_oApp.ADMIN_MODE;
+
+    //if m_oApp.ADMIN_MODE then
+    begin
+      edUser.Text := (m_oApp as TDtTstAppDb).DB.ConnectUser;
+      edPw  .Text := (m_oApp as TDtTstAppDb).DB.ConnectPassword;
+    end;
+
+    if chbAutoLogin.Checked then
+    begin
+
+      btnLogin.Click();
+
+      if not btnLogin.Enabled then
+      begin
+        Result := mrOk;
+        Exit;
+      end;
+
+    end;
+
+    Result := inherited ShowModal();
+
+  finally
+      m_oApp.LOG.m_lbLogView := nil;
+  end;
+end;
+
+procedure TFrmFDB.tsDevChange(Sender: TObject; NewTab: Integer;
+  var AllowChange: Boolean);
+begin
+
+  if not m_oApp.ADMIN_MODE then Exit;
+
+  lbLog.Visible := (NewTab = 0);
+
+  btnSqlOpen        .Visible := (NewTab = 1);
+  btnSqlOpenSample  .Visible := (NewTab = 1);
+  btnIsqlExec       .Visible := (NewTab = 1);
+  btnIsqlExecSample .Visible := (NewTab = 1);
+  edTerm            .Visible := (NewTab = 1);
+  moSql             .Visible := (NewTab = 1);
+
+end;
+
+end.
