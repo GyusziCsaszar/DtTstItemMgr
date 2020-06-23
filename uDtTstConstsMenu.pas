e77df11edd4dc30968940066bf695c24f0373b71
GyusziCsaszar <gyuszi.csaszar@gmail.com>
2020-06-23 14:58:41 +0200

    Initial commit.

---
+++ uDtTstConstsMenu.pas
@@ -0,0 +1,51 @@
+unit uDtTstConstsMenu;
+
+interface
+
+const
+
+  { App - Menu }
+  ccMnuBtn                     = '0';
+  ccMnuGrp                     = '1';
+  ccMnuItm                     = '2';
+  ccMnuCap                     = '3';
+  // --------------------------------
+  ciMnuIdx_Ctrl                = 1;
+  ciMnuIdx_ID                  = 2;
+  ciMnuCch                     = 4;
+  // --------------------------------
+  ciMnuGrpItmLeftMargin        = 8; //13;
+  // --------------------------------
+  ccMnuGrpID_Database          = '1';
+  //
+  ccMnuItmID_View              = '2';
+  ccMnuItmID_Table             = '3';
+  ccMnuItmID_Table_Column      = '4';
+  ccMnuItmID_Table_Trigger     = '5';
+  ccMnuItmID_Table_Constraint  = '6';
+  ccMnuItmID_Query             = '7';
+  // --------------------------------
+  ccMnuBtnID_Tables            = '8';
+  ccMnuBtnID_Tables_Back       = '9';
+  //
+  ccMnuBtnID_Refresh           = 'A';
+  ccMnuBtnID_Details           = 'B';
+  ccMnuBtnID_Open              = 'C';
+  ccMnuBtnID_Import_Table      = 'D';
+  ccMnuBtnID_Delete_From_Table = 'E';
+  ccMnuBtnID_Drop_Table        = 'F';
+  ccMnuBtnID_Drop_Column       = 'G';
+  ccMnuBtnID_Drop_View         = 'H';
+  //
+  ccMnuBtnID_GrdRefresh        = 'I';
+  ccMnuBtnID_GrdDelete         = 'J';
+  ccMnuBtnID_GrdInsert         = 'K';
+  // { Product }
+  ccMnuBtnID_Import_Item_Type  = 'L';
+  ccMnuBtnID_Import_Item_v1    = 'M';
+  ccMnuBtnID_Import_Item       = 'N';
+  ccMnuBtnID_Import_Item_Group = 'O';
+
+implementation
+
+end.
